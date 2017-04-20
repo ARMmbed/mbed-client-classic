404178e7f86f14f48487ca13be914dccc24774bf
Jaakko Kukkohovi <jaakko.kukkohovi@arm.com>
2017-04-20 14:33:44 +0300

    Don't retry when ssl connection error happens

--- source/m2mconnectionhandlerpimpl.cpp
+++ source/m2mconnectionhandlerpimpl.cpp
@@ -588,7 +588,7 @@ void M2MConnectionHandlerPimpl::receive_handshake_handler()
 
             _handshake_retry = 0;
             _is_handshaking = false;
-            _observer.socket_error(M2MConnectionHandler::SSL_CONNECTION_ERROR, true);
+            _observer.socket_error(M2MConnectionHandler::SSL_CONNECTION_ERROR, false);
             close_socket();
 
         }
@@ -598,7 +598,7 @@ void M2MConnectionHandlerPimpl::receive_handshake_handler()
 
                 _handshake_retry = 0;
                 _is_handshaking = false;
-                _observer.socket_error(M2MConnectionHandler::SSL_CONNECTION_ERROR, true);
+                _observer.socket_error(M2MConnectionHandler::SSL_CONNECTION_ERROR, false);
                 close_socket();
 
             }
